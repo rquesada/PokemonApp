4883615268f9f9e99aa826c73e9264bf14e94a6f
Roy Quesada <roy.quesada@gmail.com>
2023-05-06 13:43:34 -0600

    Empty project

---
+++ PokemonApp/PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Roy Quesada on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
